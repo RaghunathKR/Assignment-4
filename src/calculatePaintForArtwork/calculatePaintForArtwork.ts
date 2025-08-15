24916c65259128c817a74cf760349bdc67c5f94a
Raghunath K R <raghunathnkl88@gmail.com>
2025-08-14 21:36:12 -0400

    code written for test cases

--- src/calculatePaintForArtwork/calculatePaintForArtwork.ts
+++ src/calculatePaintForArtwork/calculatePaintForArtwork.ts
@@ -3,3 +3,35 @@ import paintRequiredForMultipleCoats from "../paintRequiredForMultipleCoats/pain
 import calculatePaintCost from "../calculatePaintCost/calculatePaintCost";
 import calculateTotalCost from "../calculateTotalCost/calculateTotalCost";
 
+export default function calculatePaintForArtwork(
+  length: number,
+  width: number,
+  copies: number,
+  coats: number,
+  coveragePerLiter: number,
+  costPerLiter: number,
+  laborCost: number
+) {
+  // 1. Total area
+  const totalArea = calculateArtworkArea(length, width, copies);
+
+  // 2. Paint required for given coats
+  const paintRequired = paintRequiredForMultipleCoats(
+    totalArea,
+    coveragePerLiter,
+    coats
+  );
+
+  // 3. Paint cost
+  const paintCost = calculatePaintCost(paintRequired, costPerLiter);
+
+  // 4. Total cost (paint + labor)
+  const totalCost = calculateTotalCost(paintCost, laborCost);
+
+  return {
+    totalArea,
+    paintRequired,
+    paintCost,
+    totalCost
+  };
+}
